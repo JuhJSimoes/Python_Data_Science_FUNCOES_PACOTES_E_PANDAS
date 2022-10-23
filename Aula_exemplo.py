51db3d7f93b926fa1624319fef08636a7e4a3f33
JuhJSimoes <julianajsimoes@gmail.com>
2022-10-23 10:50:09 -0300

    Finalizando o modulo Conhecendo a linguagem e principais bibliotecas

--- Aula_exemplo.py
+++ Aula_exemplo.py
@@ -1,7 +1,3 @@
-import re
-from unittest import result
-
-
 dados = {
     'Crossfox': {'km': 35000, 'ano': 2005}, 
     'DS5': {'km': 17000, 'ano': 2015}, 
@@ -55,4 +51,37 @@ def km_media(dataset, ano_atual):
     return result
 
 resultado = km_media(dados, 2019)
-print(resultado)
+print(resultado, '\n')
+
+
+import pandas as pd
+
+dados = {
+    'Motor': ['Motor 4.0 Turbo', 'Motor Diesel', 'Motor Diesel V8', 'Motor 2.0', 'Motor 1.6'],
+    'Ano': [2019, 2003, 1991, 2019, 1990],
+    'Quilometragem': [0.0, 5712.0, 37123.0, 0.0, 120000.0],
+    'Zero_km': [True, False, False, True, False],
+    'Valor': [88000.0, 106000.0, 72000.0, 89000.0, 32000.0]
+}
+
+dataset = pd.DataFrame(dados, index = ['Jetta', 'Passat', 'Crossfox', 'DS5', 'Fusca'])
+
+print(dataset.iloc[[1,3], [0,1]])
+print(dataset.loc[['Passat', 'DS5'], ['Motor', 'Valor']], '\n')
+
+
+
+dados = {
+    'Motor': ['Motor 4.0 Turbo', 'Motor Diesel', 'Motor Diesel V8', 'Motor Diesel', 'Motor 1.6'],
+    'Ano': [2019, 2003, 1991, 2019, 1990],
+    'Quilometragem': [0.0, 5712.0, 37123.0, 0.0, 120000.0],
+    'Zero_km': [True, False, False, True, False],
+    'Valor': [88000.0, 106000.0, 72000.0, 89000.0, 32000.0]
+}
+
+dataset = pd.DataFrame(dados, index = ['Jetta', 'Passat', 'Crossfox', 'DS5', 'Fusca'])
+
+dataset.query('Motor == "Motor Diesel" or Zero_km == True')
+dataset[(dataset.Motor == "Motor") | (dataset.Zero_km == True)]
+dataset.query('Motor == "Motor Diesel" | Zero_km == True')
+
